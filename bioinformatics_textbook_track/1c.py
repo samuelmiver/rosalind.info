2dc6efd86a9496566c76cd8772c8c9f3a5e10839
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-02-05 12:31:25 +0100

    new problem solved 20150205

---
+++ bioinformatics_textbook_track/1c.py
@@ -0,0 +1,18 @@
+def pattern_matching(filename):
+	"""Given a short string and a whole sequence, retrieve the
+	indexes where the submotif appears """
+
+	with open(filename) as filehandle:
+		subseq = filehandle.readline().strip()
+		seq = filehandle.readline()
+
+	locations = []
+	i = seq.find(subseq, 0)
+	while i != -1:
+		locations.append(i)
+		i += 1
+		i = seq.find(subseq, i)
+	return '%s' % ' '.join(map(str, locations))
+
+
+print (pattern_matching("./files/rosalind_1c.txt"))
