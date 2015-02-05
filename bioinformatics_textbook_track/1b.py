2eddb83d6b4413beb78447c183990b943d27a337
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-02-05 12:18:32 +0100

    new problem solved 20150205

---
+++ bioinformatics_textbook_track/1b.py
@@ -0,0 +1,15 @@
+def reverse_complement(filename):
+    """Given a string returns the reverse complementary"""
+
+    with open(filename) as filehandle:
+        seq = filehandle.readline().strip()
+
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+
+    bases = list(seq) 
+    bases = reversed([complement.get(base,base) for base in bases])
+    bases = ''.join(bases)
+    print(bases)
+
+
+reverse_complement("./files/rosalind_1b.txt")
