7fd1aadb27d22e504299e88c80faae53f66b04e4
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-08-11 17:18:17 +0200

    Minimum Skew Problem

---
+++ bioinformatics_textbook_track/1fba.py
@@ -0,0 +1,18 @@
+with open('files/rosalind_1fba.txt') as input_data:
+	dna = input_data.read().strip()
+
+skew_value, min_skew, min_ind = 0, 1, []
+for index, nucleotide in enumerate(dna):
+	
+	if nucleotide == 'C':
+		skew_value -= 1
+	elif nucleotide == 'G':
+		skew_value += 1
+	
+	if skew_value == min_skew:
+		min_ind.append(str(index+1))
+	elif skew_value < min_skew:
+		min_skew = skew_value
+		min_ind = [str(index+1)]
+
+print ' '.join(min_ind)
