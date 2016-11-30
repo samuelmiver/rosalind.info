eed16fff350b5285de7a697f99d7f28371e7589c
Samuel Miravet Verde <samuelmiver@gmail.com>
2016-11-30 12:39:06 +0100

    lexf

--- bioinformatics_stronghold/lexf.py
+++ bioinformatics_stronghold/lexf.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 
 from itertools import product
-from string import ascii_lowercase
 
 def lexf(inFile):
 
     # Process the file
-    l = []
-    alphabet = None
-    with open(inFile, 'r') as fi:
-        for line in fi:
-            line = line.strip()
-            if not alphabet:
-                alphabet = line.replace(' ','')
-            else:
-                n = int(line)
+    with open(inFile) as fi:
+        alphabet, n = fi.readlines()
+        alphabet    = ''.join(alphabet.split())
+        n           = int(n)
 
     # Generate all the possible subsequences
-    print '\n'.join([''.join(i) for i in product(alphabet, repeat = n)])
+    print ' '.join(sorted([''.join(i) for i in product(alphabet, repeat = n)]))
 
 if __name__ == '__main__':
     lexf('./files/rosalind_lexf.txt')
