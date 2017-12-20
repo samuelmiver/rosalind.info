2e0055263fc96c9defd52a3929a94a97af2a3fe0
Samuel Miravet Verde <samuelmiver@gmail.com>
2017-12-20 16:18:57 +0100

    filt

--- bioinformatics_armory/filt.py
+++ bioinformatics_armory/filt.py
@@ -3,30 +3,24 @@
 import math
 import sys
 import os
-from Bio import SeqIO
 
 def filt():
 
     with open('files/rosalind_filt.txt') as fi:
-        q, p = map(int, fi.readline().split())
+        q, p = map(str, fi.readline().split())
         fastq = fi.read()
 
-    with open('tmp.fastq', 'w') as fo:
+    with open('files/tmp.fastq', 'w') as fo:
         fo.write(fastq)
 
-    number_sequences_filtered = 0
-    records = SeqIO.parse('tmp.fastq','fastq')
+    # Run fastq quality filter
+    cmd = 'fastq_quality_filter -q '+q+' -p '+p+' -Q33 -i files/tmp.fastq -o files/filtered.fastq'
+    os.system(cmd)
 
-    for record in records:
-        qualities = record.letter_annotations['phred_quality']
-        number_bases_above_qthres = sum([1 if b >= q else 0 for b in qualities])
-
-        if number_bases_above_qthres < len(qualities) * p / 100.0:
-            number_sequences_filtered += 1
-
-    print number_sequences_filtered
+    num_lines = sum(1 for line in open('files/filtered.fastq'))
+    print num_lines/4
 
 if __name__ == '__main__':
     filt()
 
-    os.system('rm tmp.fastq')
+    os.system('rm files/tmp.fastq files/filtered.fastq')
